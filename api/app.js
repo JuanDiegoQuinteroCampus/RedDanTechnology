7453903b040c2ebad05b170fd8e4a865aff5a8f0
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2024-01-19 01:46:24 -0500

    feat: :sparkles: Implementation of the tables's CRUD
    Implemented the functions that allow interaction with the cruds, with the help of the url

--- api/app.js
+++ api/app.js
@@ -1,11 +1,17 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import appClientes from './routers/clientes.js';
+import appProducto from './routers/producto.js';
+import appFactura from './routers/factura.js';
 
 dotenv.config();
 const app = express()
 
 app.use(express.json());
 
+app.use('/clientes', appClientes);
+app.use('/producto', appProducto);
+app.use('/factura', appFactura);
 
 
 const config = JSON.parse(process.env.MY_CONFIG);
