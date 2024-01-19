bb358f3d71e6e374ee819c91f52b459a5e103a8c
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2024-01-19 14:10:56 -0500

    feat: :sparkles: Add file export
    The export file has been added to make the export of the table functional.

---
+++ src/components/Export.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Exportar a PDF</h1>
+    <button @click="exportarAPDF">Exportar a PDF</button>
+  </div>
+</template>
+
+<script setup>
+const exportarAPDF = async () => {
+  try {
+    const response = await fetch('http://localhost:5023/pdf/exportopdf');
+    const blob = await response.blob();
+
+    const url = window.URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'clientes.pdf';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  } catch (error) {
+    console.error('Error al exportar a PDF:', error);
+  }
+};
+</script>
