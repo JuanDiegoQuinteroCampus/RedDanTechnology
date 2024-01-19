941b90c49b4353135ff6c135920503067956119f
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2024-01-19 01:17:28 -0500

    feat: :card_file_box: Changes in database and add new data

--- api/db/db_RedDanTechnology.sql
+++ api/db/db_RedDanTechnology.sql
@@ -18,7 +18,7 @@ CREATE TABLE factura(
 num_factura INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
 id_cliente INT NOT NULL,
 fecha DATE NOT NULL,
-id_producto INT NOT NULL,
+id_producto INT,
 FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
 );
 
@@ -27,9 +27,47 @@ id_producto INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 nombre VARCHAR(255) NOT NULL,
 precio INT NOT NULL,
 stock INT NOT NULL,
-id_factura INT NOT NULL,
+id_factura INT ,
 FOREIGN KEY (id_factura) REFERENCES factura(num_factura)
 );
 
 ALTER TABLE factura
 ADD FOREIGN KEY (id_producto) REFERENCES producto(id_producto);
+
+
+INSERT INTO cliente (nombre, apellido, direccion, fecha_nacimiento, telefono, email, categoria)
+VALUES ('Juan', 'Perez', 'Calle 123', '1990-01-01', 123456789, 'juan@example.com', 'Categoria A');
+
+INSERT INTO cliente (nombre, apellido, direccion, fecha_nacimiento, telefono, email, categoria)
+VALUES ('Maria', 'Gomez', 'Avenida 456', '1985-05-15', 987654321, 'maria@example.com', 'Categoria B');
+
+
+
+
+INSERT INTO producto (nombre, precio, stock)
+VALUES ('Producto A', 50, 100);
+
+INSERT INTO producto (nombre, precio, stock)
+VALUES ('Producto B', 75, 50);
+
+
+
+
+INSERT INTO factura (id_cliente, fecha,id_producto)
+VALUES (1, '2024-01-18',1);
+
+INSERT INTO factura (id_cliente, fecha,id_producto)
+VALUES (2, '2024-01-19',2);
+
+
+
+
+
+
+
+
+
+
+
+ 
+/* DESCRIBE factura; */
